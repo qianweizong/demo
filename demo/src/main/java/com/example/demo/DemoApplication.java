d8f87cb83b265d6fb649c8e6ec735f6b26d4e889
qianweizong <506040935@qq.com>
2020-07-23 11:43:32 +0800

    提交

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//112
+
 @SpringBootApplication
 public class DemoApplication {
 
